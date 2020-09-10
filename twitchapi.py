cb28b60cb93ad9ed93846fa0e80efd9d039003f7
AlejoGB <alejo.garcia@rfindustrial.com>
2020-09-10 20:03:56 -0300

    get oauth tkn

--- twitchapi.py
+++ twitchapi.py
@@ -1,27 +1,49 @@
 import requests
 import logging
 from datetime import datetime
+import json
 
 
 
 class TwitchAPI:
-    def __init__(self, client, verbose=False, log=False, *args, **kwargs):
-
+    def __init__(self, verbose=False, log=False, timeout=10,*args, **kwargs):
+        
+        self.timeout = timeout
+        self.urls = self.get_urls('urls.json')# endpoints
         self.verbose = verbose
-        self.log = 'logging.'+log # INFO DEBUG WARNING ERROR CRITICAL
+        if log:
+            self.log = 'logging.'+ kwargs.get('logger','error').upper() # INFO DEBUG WARNING ERROR CRITICAL
+        self.last_status = 0
 
-
-        logging.basicConfig(format='%(lineno)d - %(asctime)s -%(filename)s - %(levelname)s : %(message)s'
-                                            level=logging.INFO,
-                                            filename = 'loggerTemplate.log', 
+        logging.basicConfig(format='%(lineno)d - %(asctime)s -%(filename)s - %(levelname)s : %(message)s',
+                                            level=eval(self.log),
+                                            filename='logs/api.log', 
                                             #, filemode = 'w'
                                             #,  
                                             )
+
+    def get_urls(self, path):
+        with open(path) as f:
+            return json.load(f)
+
+    
+    def get_tkn(self, data):
+        try:
+            response = requests.post(self.urls['oauth_token'] ,data=data, timeout=self.timeout)
+        except Exception as e:
+            self.print('Request stopped by an exception: {}'.format(e))
+            logging.error('Request stopped by an exception: {}'.format(e))
+            return False 
+        self.print('Token request returned with status code > {}'.format(response.status_code))
+        logging.info('Token request returned with status code > {}'.format(response.status_code))
+        self.last_status = response.status_code
+        return response
 
 
     def print(self, string=''):
         if self.verbose:
             print(string)
+        pass
             
 
     def make_request(self, url):
@@ -32,7 +54,9 @@ class TwitchAPI:
                 url=url,
                 timeout=self.timeout,
                 headers={'Client-ID': self.client_id})
-        except Exceptions as e:
+        except Exception as e:
+            self.print('Request stopped by an exception: {}'.format(e))
+            logging.error('Request stopped by an exception: {}'.format(e))
             return False
 
         self.last_status = response.status_code
@@ -42,13 +66,22 @@ class TwitchAPI:
             json_data = response.json()
         except json.decoder.JSONDecodeError as e:
             self.print('Error parsing JSON: {}'.format(e))
-            logging.error('Error parsing JSON: {}'.format(e))
+            logging.error('parsing JSON: {}'.format(e))
             return False
 
         return json_data
 
     class games:
         def __init__(self, *args, **kwargs):
+            pass
 
         def top():
-            response = self.make_request()
+            response = self.make_request(self.urls['games']['top'])
+            if response:
+                return resoponse
+            else:
+                logging.error('No data retrieved from TOP GAMES request')
+                raise Exception('No data retrieved from request')
+            pass
+            
+            
